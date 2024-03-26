63101f5b42ebe258681e93b9338f03c4582890d9
hnasreen <hnasreen1993@gmail.com>
2024-03-26 20:12:50 +0530

    Day 13 DOM Day 1 Task

---
+++ script.js
@@ -0,0 +1,31 @@
+// let container=document.createElement("div");
+// container.setAttribute("class","container")
+
+// let row=document.createElement("div");
+// row.setAttribute("class","row")
+
+// let col=document.createElement("div")
+// col.innerHTML="This is col"
+// col.setAttribute("class","col")
+
+// row.append(col)
+// container.append(row)
+// document.body.append(container)
+
+function foo(){
+    let fname=document.getElementById("FirstName").value;
+    let mname=document.getElementById("MiddleName").value;
+    let lname=document.getElementById("LastName").value;
+    let email=document.getElementById("email").value;
+    let address=document.getElementById("Address").value;
+    console.log(fname);
+    console.log(mname);
+    console.log(lname);
+    console.log(email);
+    console.log(address);
+}
+
+
+
+
+
